30812dc0aa582b3dc32c66a080d078c510f87c71
YuuichiSama <142111069+YuuichiSama@users.noreply.github.com>
2023-12-07 13:10:59 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -22,14 +22,8 @@ START_IMG = [
 HEY_IMG = "https://graph.org/file/f32dea55756c9ffa1c2ca.jpg"
 
 ALIVE_ANIMATION = [
-    "https://telegra.ph//file/f9e2b9cdd9324fc39970a.mp4",
-    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
-    "https://telegra.ph//file/c4c2759c5fc04cefd207a.mp4",
-    "https://telegra.ph//file/b1fa6609b1c4807255927.mp4",
-    "https://telegra.ph//file/f3c7147da6511fbe27c25.mp4",
-    "https://telegra.ph//file/39071b73c02e3ff5945ca.mp4",
-    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
-    "https://telegra.ph//file/6efdd8e28756bc2f6e53e.mp4",
+    "https://telegra.ph/file/b384e7540a2e5f28a2dd1.mp4",
+    "https://telegra.ph/file/2243fb96ef5a4aeaf2ad3.mp4",
 ]
 
 BAN_GIFS = [
